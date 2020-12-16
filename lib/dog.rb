9b41ebd73de490f3cc7d37d89e3dda3292ec8a51
Diana Rybikov <diana.ryb214@gmail.com>
2020-12-16 11:01:32 -0700

    commit tree 167e506a433889893671c6932977fca1eee6e17f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+require 'pry'
+class Dog
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize name
+        @name = name
+            save
+    end
+    
+    def self.all
+    @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        all.find_all do |dogs|
+            puts dogs.name
+    end
+end
+    def save
+        @@all << self
+    end
+
+end
+
+pluto = Dog.new("Pluto")
+maddy = Dog.new("Maddy")
+fido = Dog.new("Fido")
+
+# binding.pry
